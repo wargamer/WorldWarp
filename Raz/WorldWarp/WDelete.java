75845332064dcca471505f5ffd8f680293634c43
Matt Boulanger <celeodor@gmail.com>
2012-01-05 14:53:27 -0800

    Removing extra files

--- Raz/WorldWarp/WDelete.java
+++ Raz/WorldWarp/WDelete.java
@@ -1,32 +1,32 @@
-package Raz.WorldWarp;
+package raz.worldwarp;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.util.config.Configuration;
 
-public class WDelete
-{
-  public WDelete(Player p, String[] args, Configuration Worlds, Configuration Config)
-  {
-    if (args.length < 1) {
-      p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + "Use /wdelete [name].");
-    } else {
-      Worlds.load();
-      if (p.getServer().getWorld(args[0]) != null) {
-        Worlds.removeProperty("worlds." + args[0]);
-        Worlds.save();
-        p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + args[0] + " Deleted from worlds.yml.");
-        p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + "Please delete the folder manually.");
-        p.getServer().unloadWorld(args[0], true);
-      } else {
-        p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + args[0] + " Is not a loaded world.");
-      }
-    }
-  }
-}
+@SuppressWarnings("deprecation")
+public class WDelete {
 
-/* Location:           C:\Users\Matt\Desktop\WorldWarp.jar
- * Qualified Name:     Raz.WorldWarp.WDelete
- * JD-Core Version:    0.6.0
- */
+	public WDelete(Player p, String[] args, Configuration Worlds,
+			Configuration Config) {
+		if (args.length < 1) {
+			p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED
+					+ "Use /wdelete [name].");
+		} else {
+			Worlds.load();
+			if (p.getServer().getWorld(args[0]) != null) {
+				Worlds.removeProperty("worlds." + args[0]);
+				Worlds.save();
+				p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: "
+						+ ChatColor.RED + args[0] + " Deleted from worlds.yml.");
+				p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: "
+						+ ChatColor.RED + "Please delete the folder manually.");
+				p.getServer().unloadWorld(args[0], true);
+			} else {
+				p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: "
+						+ ChatColor.RED + args[0] + " Is not a loaded world.");
+			}
+		}
+	}
+
+}
--- Raz/WorldWarp/WWarp.java
+++ Raz/WorldWarp/WWarp.java
@@ -1,32 +1,31 @@
-package Raz.WorldWarp;
+package raz.worldwarp;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Server;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.util.config.Configuration;
 
-public class WWarp
-{
-  public WWarp(Player p, String[] args, Configuration Worlds, Configuration Config)
-  {
-    if (args.length < 1) {
-      p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + "Use /wwarp [name].");
-      return;
-    }
-    if (p.getServer().getWorld(args[0]) != null) {
-      Location loc = p.getServer().getWorld(args[0]).getSpawnLocation();
-      p.teleport(loc);
-      p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.GREEN + "Welcome to " + args[0]);
-      return;
-    }
-    p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + "That world does not exist.");
-    p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED + "Use /wlist to see worlds.");
-  }
-}
+@SuppressWarnings("deprecation")
+public class WWarp {
 
-/* Location:           C:\Users\Matt\Desktop\WorldWarp.jar
- * Qualified Name:     Raz.WorldWarp.WWarp
- * JD-Core Version:    0.6.0
- */
+	public WWarp(Player p, String[] args, Configuration Worlds,
+			Configuration Config) {
+		if (args.length < 1) {
+			p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED
+					+ "Use /wwarp [name].");
+			return;
+		}
+		if (p.getServer().getWorld(args[0]) != null) {
+			Location loc = p.getServer().getWorld(args[0]).getSpawnLocation();
+			p.teleport(loc);
+			p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: "
+					+ ChatColor.GREEN + "Welcome to " + args[0]);
+			return;
+		}
+		p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED
+				+ "That world does not exist.");
+		p.sendMessage(ChatColor.DARK_RED + "[WorldWarp]: " + ChatColor.RED
+				+ "Use /wlist to see worlds.");
+	}
+
+}
